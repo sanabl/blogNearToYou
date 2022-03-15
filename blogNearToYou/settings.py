5142f54bfdd68d99175cc7ac259634dab4a4dc99
sanabl <sanabeltahon98@gmail.com>
2022-03-15 08:52:28 +0200

    add database connection and requirements

--- blogNearToYou/settings.py
+++ blogNearToYou/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blogNearToYou.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'DataBase': 'blog',
+        'User':'root',
+        'Password':'root',
+        'default-character-set': 'utf8'
+
     }
 }
 
