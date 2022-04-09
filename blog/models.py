3591dd73dbf031eaead5e5963089e39075badcee
sanabl <sanabeltahon98@gmail.com>
2022-04-09 22:52:57 +0300

    create post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
-from importlib.resources import _
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
@@ -11,3 +11,10 @@ class User(AbstractUser):
 class UserProfile(models.Model):
     nickname = models.CharField(max_length=30)
     User = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    published = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
